6089ae5d24a61e02748ad47b5b4d6ebbfac739c3
plissken2013es <luis_jose.quintana_vazquez.ext@on.nokia.com>
2016-08-02 15:47:15 +0200

    Chapter 3 patched (\n issue) and finished

--- js/action_methods.js
+++ js/action_methods.js
@@ -3,15 +3,15 @@ GWBW.action_methods = {
         var daysLeft = 40 - this.day;
         var resultText = "Por la posición de las lunas, calculo que ";
         if (daysLeft > 30) {
-            resultText += "falta muchísimo\nhasta que llegue el Convoy.";
+            resultText += "falta muchísimo \n hasta que llegue el Convoy.";
         } else if (daysLeft > 20) {
-            resultText += "estamos cercanos\nal Ecuador de nuestra desgraciada aventura.";
+            resultText += "estamos cercanos \n al Ecuador de nuestra desgraciada aventura.";
         } else if (daysLeft > 10) {
-            resultText += "hemos entrado\nen la recta final.";
+            resultText += "hemos entrado \n en la recta final.";
         } else if (daysLeft > 5) {
-            resultText += "se nos está\nacabando el tiempo. Faltan pocos días.";
+            resultText += "se nos está \n acabando el tiempo. Faltan pocos días.";
         } else {
-            resultText += "casi puedo\ntocarlo con los dedos...";
+            resultText += "casi puedo \n tocarlo con los dedos...";
         }
         this.dialogbox.text = resultText;
         this.dialogbox.name = 'Sgt Burden';
@@ -52,11 +52,11 @@ GWBW.action_methods = {
     },
     radioAction: function() {
         if (this.radioStatus >= this.radioMax) {
-            this.dialogbox.text = '¡Por fin! La radio funciona. Espero que puedan\noír nuestro aviso de socorro.';
+            this.dialogbox.text = '¡Por fin! La radio funciona. Espero que puedan \n oír nuestro aviso de socorro.';
             this.dialogbox.name = 'Sgt Burden';
             this.tweenDialog({ y: 0 }, 1);
         } else {
-            this.dialogbox.text = 'Es nuestra única salida, deberíamos arreglarla\nantes del día 40.';
+            this.dialogbox.text = 'Es nuestra única salida, deberíamos arreglarla \n antes del día 40.';
             this.dialogbox.name = 'Sgt Burden';
             this.tweenDialog({ y: 0 }, 1);
         }
@@ -96,7 +96,7 @@ GWBW.action_methods = {
     },
     doctorAction2: function() {
         var vaccines = this.math.between(0, 2);
-        var txt = 'Conozco algunas hierbas de este planeta que podrán sernos útiles.\n';
+        var txt = 'Conozco algunas hierbas de este planeta que podrán sernos útiles. \n ';
         if (vaccines) {
             txt += '[¡El Doctor ha preparado ' + vaccines + ' vacuna/s!]';
         } else {
@@ -115,7 +115,7 @@ GWBW.action_methods = {
             this.dialogbox.name = 'Doctor';
             this.tweenDialog({ y: 0 }, 1);
         } else {
-            this.dialogbox.text = 'Vale, veré qué puedo hacer...\n[El doctor ha reparado un 2% de la radio.]';
+            this.dialogbox.text = 'Vale, veré qué puedo hacer... \n [El doctor ha reparado un 2% de la radio.]';
             this.dialogbox.name = 'Doctor';
             this.tweenDialog({ y: 0 }, 1, function() {
                 this.radioStatus += 1;
@@ -128,19 +128,19 @@ GWBW.action_methods = {
             this.shootCrew(this.DOCTOR_ID);
             this.ammo--;
         } else {
-            this.dialogbox.text = 'No nos queda munición. Y no me veo capaz de \nmatarlo con una roca o algo así...';
+            this.dialogbox.text = 'No nos queda munición. Y no me veo capaz de  \n matarlo con una roca o algo así...';
             this.dialogbox.name = 'Sgt Burden';
             this.tweenDialog({ y: 0 }, 1);
         }
     },
     doctorAction5: function() {
-        this.dialogbox.text = "¿Doctor? ¿Está usted bien? ...\nCreo que puede estar infectado por el virus Medusea.";
+        this.dialogbox.text = "¿Doctor? ¿Está usted bien? ... \n Creo que puede estar infectado por el virus Medusea.";
         this.dialogbox.name = "Sgt Burden";
         this.tweenDialog({ y: 0 }, 1);
     },
     doctorAction6: function() {
         if (this.medicines > 0) {
-            this.dialogbox.text = '¿Qu-qué ha pasado? Oh, ya veo... debo de haberme infectado con el\nvirus Medusea, gracias por salvarme.';
+            this.dialogbox.text = '¿Qu-qué ha pasado? Oh, ya veo... debo de haberme infectado con el \n virus Medusea, gracias por salvarme.';
             this.dialogbox.name = 'Doctor';
             this.tweenDialog({ y: 0 }, 1, function() {
                 this.medicines--;
@@ -164,7 +164,7 @@ GWBW.action_methods = {
             this.rationsNeeded++;
 
         } else {
-            this.dialogbox.text = 'No nos queda munición. Y no me veo capaz de \nmatarlo con una roca o algo así...';
+            this.dialogbox.text = 'No nos queda munición. Y no me veo capaz de  \n matarlo con una roca o algo así...';
             this.dialogbox.name = 'Sgt Burden';
             this.tweenDialog({ y: 0 }, 1);
         }
@@ -182,7 +182,7 @@ GWBW.action_methods = {
     },
     scientistAction2: function() {
         if (this.radioStatus >= this.radioMax) {
-            this.dialogbox.text = 'El trabajo está hecho, jefe. Sólo mantengámonos con vida\nhasta que llegue el Convoy Orbital.';
+            this.dialogbox.text = 'El trabajo está hecho, jefe. Sólo mantengámonos con vida \n hasta que llegue el Convoy Orbital.';
             this.dialogbox.name = 'Ingeniero';
             this.dialogbox.tween({
                 pos: {
@@ -192,7 +192,7 @@ GWBW.action_methods = {
                 easing: ig.Tween.Easing.Quadratic.EaseOut
             }).start();
         } else {
-            this.dialogbox.text = 'Vamos a reparar esta chatarra.\n[El ingeniero ha reparado un 6% de la radio.]';
+            this.dialogbox.text = 'Vamos a reparar esta chatarra. \n [El ingeniero ha reparado un 6% de la radio.]';
             this.dialogbox.name = 'Ingeniero';
             this.tweenDialog({ y: 0 }, 1, function() {
                 this.radioStatus += 3;
@@ -211,13 +211,13 @@ GWBW.action_methods = {
         }
     },
     scientistAction4: function() {
-        this.dialogbox.text = 'Ey, colega, ¿estás meditando o ...?\n¡Oh, mierda...! Se ha infectado.';
+        this.dialogbox.text = 'Ey, colega, ¿estás meditando o ...? \n ¡Oh, mierda...! Se ha infectado.';
         this.dialogbox.name = 'Sgt Burden';
         this.tweenDialog({ y: 0 }, 1);
     },
     scientistAction5: function() {
         if (this.medicines > 0) {
-            this.dialogbox.text = 'He tenido un sueño: estaba cenando en casa de mi madre...\nGracias por salvarme, por cierto.';
+            this.dialogbox.text = 'He tenido un sueño: estaba cenando en casa de mi madre... \n Gracias por salvarme, por cierto.';
             this.dialogbox.name = 'Ingeniero';
             this.tweenDialog({ y: 0 }, 1,  function() {
                 this.medicines--;
@@ -264,8 +264,8 @@ GWBW.action_methods = {
             if (Math.random() > .1) {
                 var food = this.math.between(5, 8);
                 this.foodAmount += food;
-                var resultText = 'El soldado ha gastado ' + numShots + ' bala(s) y ha obtenido\n' + food + ' raciones de alimentos.';
-                if (food == 1) resultText = 'El soldado ha gastado ' + numShots + ' bala(s) y ha obtenido\nuna ración de comida.';
+                var resultText = 'El soldado ha gastado ' + numShots + ' bala(s) y ha obtenido \n ' + food + ' raciones de alimentos.';
+                if (food == 1) resultText = 'El soldado ha gastado ' + numShots + ' bala(s) y ha obtenido \n una ración de comida.';
                 this.dialogbox.text = resultText;
                 this.dialogbox.name = 'Resultados de la caza';
                 this.tweenDialog({ y: 0 }, 1, function() {
@@ -279,7 +279,7 @@ GWBW.action_methods = {
                 });
             }
         } else {
-            this.dialogbox.text = 'Nos hemos quedado sin munición, lo siento. Si queda alguna bala,\nutilícela para destruir al robot. Podríamos usar sus baterías.';
+            this.dialogbox.text = 'Nos hemos quedado sin munición, lo siento. Si queda alguna bala, \n utilícela para destruir al robot. Podríamos usar sus baterías.';
             this.dialogbox.name = 'Soldado';
             this.tweenDialog({ y: 0 }, 1);
         }
@@ -289,19 +289,19 @@ GWBW.action_methods = {
             this.shootCrew(this.SOLDIER_ID);
             this.ammo--;
         } else {
-            this.dialogbox.text = 'Me he quedado sin balas... y no voy a poder matarlo\ncon las manos desnudas.';
+            this.dialogbox.text = 'Me he quedado sin balas... y no voy a poder matarlo \n con las manos desnudas.';
             this.dialogbox.name = 'Sgt Burden';
             this.tweenDialog({ y: 0 }, 1);
         }
     },
     soldierAction4: function() {
-        this.dialogbox.text = '¿Has visto algo? Mantene alerta.\n¿Hola? ¿Estás ahí? Oh... el virus Medusea...';
+        this.dialogbox.text = '¿Has visto algo? Mantene alerta. \n ¿Hola? ¿Estás ahí? Oh... el virus Medusea...';
         this.dialogbox.name = 'Sgt Burden';
         this.tweenDialog({ y: 0 }, 1);
     },
     soldierAction5: function() {
         if (this.medicines > 0) {
-            this.dialogbox.text = '¡Sargento! ¡Gracias por desperdiciar recursos valiosos en\neste débil soldado!';
+            this.dialogbox.text = '¡Sargento! ¡Gracias por desperdiciar recursos valiosos en \n este débil soldado!';
             this.dialogbox.name = 'Soldado';
             this.tweenDialog({ y: 0 }, 1, function() {
                 this.medicines--;
@@ -312,7 +312,7 @@ GWBW.action_methods = {
                 this.soldierAction.infected = false;
             });
         } else {
-            this.dialogbox.text = 'Era un gran camarada. Me gustaria salvarle.\nNecesitamos medicinas.';
+            this.dialogbox.text = 'Era un gran camarada. Me gustaria salvarle. \n Necesitamos medicinas.';
             this.dialogbox.name = 'Sgt Burden';
             this.tweenDialog({ y: 0 }, 1);
         }
@@ -324,7 +324,7 @@ GWBW.action_methods = {
             this.shootCrew(this.SOLDIER_ID);
             this.ammo--;
         } else {
-            this.dialogbox.text = 'No me queda munición... y no sería...\nhonorable matar a palos a un soldado congelado.';
+            this.dialogbox.text = 'No me queda munición... y no sería... \n honorable matar a palos a un soldado congelado.';
             this.dialogbox.name = 'Sgt Burden';
             this.tweenDialog({ y: 0 }, 1);
         }
@@ -342,7 +342,7 @@ GWBW.action_methods = {
     },
     girlAction2: function() {
         if (this.numActions >= this.numSurvivors) {
-            this.dialogbox.text = 'Este es el reto mas grande que un equipo de Eversdusk haya\nafrontado nunca. ¡Prevaleceremos y contaremos nuestra historia!';
+            this.dialogbox.text = 'Este es el reto mas grande que un equipo de Eversdusk haya \n afrontado nunca. ¡Prevaleceremos y contaremos nuestra historia!';
             this.dialogbox.name = 'Sgt Burden';
             this.tweenDialog({ y: 0 }, 1, function() {
                 for (var i = 0; i < this.sanity.length; i++) {
@@ -354,7 +354,7 @@ GWBW.action_methods = {
                 this.numActions -= this.numSurvivors;
             });
         } else {
-            this.dialogbox.text = 'Lo siento, Sargento: no nos queda tiempo para eso hoy.\n[Necesitas TODAS las acciones]';
+            this.dialogbox.text = 'Lo siento, Sargento: no nos queda tiempo para eso hoy. \n [Necesitas TODAS las acciones]';
             this.dialogbox.name = 'Psiquiatra';
             this.tweenDialog({ y: 0 }, 1);
         }
@@ -364,19 +364,19 @@ GWBW.action_methods = {
             this.shootCrew(this.GIRL_ID);
             this.ammo--;
         } else {
-            this.dialogbox.text = 'No me queda munición\ny no me veo matando a una chica a golpes.';
+            this.dialogbox.text = 'No me queda munición \n y no me veo matando a una chica a golpes.';
             this.dialogbox.name = 'Sgt Burden';
             this.tweenDialog({ y: 0 }, 1);
         }
     },
     girlAction4: function() {
-        this.dialogbox.text = 'Escucha... a veces tengo fantasías con mi nave y...\n¡Eh! ¿Me estás escuchando? Mierda, el virus Medusea.';
+        this.dialogbox.text = 'Escucha... a veces tengo fantasías con mi nave y... \n ¡Eh! ¿Me estás escuchando? Mierda, el virus Medusea.';
         this.dialogbox.name = 'Sgt Burden';
         this.tweenDialog({ y: 0 }, 1);
     },
     girlAction5: function() {
         if (this.medicines > 0) {
-            this.dialogbox.text = '¿Dónde, cómo, cuándo...?\nOh, ya veo... Gracias por salvarme, Sargento.';
+            this.dialogbox.text = '¿Dónde, cómo, cuándo...? \n Oh, ya veo... Gracias por salvarme, Sargento.';
             this.dialogbox.name = 'Psiquiatra';
             this.tweenDialog({ y: 0 }, 1, function() {
                 this.medicines--;
@@ -399,7 +399,7 @@ GWBW.action_methods = {
             this.shootCrew(this.GIRL_ID);
             this.ammo--;
         } else {
-            this.dialogbox.text = 'No me queda munición\ny no me veo matando a una chica a golpes.';
+            this.dialogbox.text = 'No me queda munición \n y no me veo matando a una chica a golpes.';
             this.dialogbox.name = 'Sgt Burden';
             this.tweenDialog({ y: 0 }, 1);
         }
@@ -445,7 +445,7 @@ GWBW.action_methods = {
         var girl = this.sanity[this.GIRL_ID] > 0 ? this.sanity[this.GIRL_ID] : "-";
 
         this.dialogbox.text = 'Psicoanálisis: ' + soldier + ', ' + doctor + ', ' + scientist + ', ' + girl;
-        this.dialogbox.text += '\nReparación de la Radio: ' + parseInt(this.radioStatus) + ' / ' + this.radioMax;
+        this.dialogbox.text += ' \n Reparación de la Radio: ' + parseInt(this.radioStatus) + ' / ' + this.radioMax;
         this.dialogbox.name = 'BR4ND-0N';
         this.tweenDialog({ y: 0 }, 1);
     },
@@ -455,7 +455,7 @@ GWBW.action_methods = {
             this.dialogbox.name = 'BR4ND-0N';
             this.tweenDialog({ y: 0 }, 1);
         } else {
-            this.dialogbox.text = 'El ingeniero debería ser más eficaz, pero haré lo que pueda.\n[BR4ND-0N ha reparado un 4% de la radio.]';
+            this.dialogbox.text = 'El ingeniero debería ser más eficaz, pero haré lo que pueda. \n [BR4ND-0N ha reparado un 4% de la radio.]';
             this.dialogbox.name = 'BR4ND-0N';
             this.tweenDialog({ y: 0 }, 1, function() {
                 this.radioStatus += 2;
